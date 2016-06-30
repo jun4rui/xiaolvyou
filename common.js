e194dff0a000833feff18238855ba29a311043b8
jun4rui <jun4rui@gmail.com>
2016-06-30 17:12:22 +0800

    加入调试代码

--- common.js
+++ common.js
@@ -8,22 +8,21 @@
  * 第二种场景：
  * 销售1->普通客户1->普通客户2   普通客户2只能联系销售1
  * 曹熙：普通用户再下次进去首页立刻清空销售人员信息
+ * 637486
  */
 var server_addr = 'http://www.xiaolvyou.com.cn';						//JSON接口服务器地址，调用接口时用，方便更换
 //var server_addr = 'http://www.96hn.com';						//JSON接口服务器地址，调用接口时用，方便更换
 
 
 //20160419 旧的销售人员逻辑用数据，即将取消
-var _INFODATA	= window.localStorage.getItem('INFO_DATA');		//_INFODATA变量保存销售人员信息用，保存在localStorage中
-var _INFOID		= '';											//_INFOID表示销售人员的系统内部ID编号
-if (_INFODATA!=null){
-	if (_INFODATA.indexOf(',')>-1){
-		_INFOID 	= _INFODATA.split(',')[3];
+var _INFODATA = window.localStorage.getItem('INFO_DATA');		//_INFODATA变量保存销售人员信息用，保存在localStorage中
+var _INFOID   = '';											//_INFOID表示销售人员的系统内部ID编号
+if (_INFODATA != null) {
+	if (_INFODATA.indexOf(',') > -1) {
+		_INFOID = _INFODATA.split(',')[3];
 	}
 }
-var _USERDATA	= JSON.parse(window.localStorage.getItem('USER_DATA'));		//_USERDATA变量保存用户信息
-
-
+var _USERDATA = JSON.parse(window.localStorage.getItem('USER_DATA'));		//_USERDATA变量保存用户信息
 
 
 //20160419 新的销售人员逻辑用数据
@@ -31,16 +30,16 @@ var _EMPLOYEE_DATA = window.localStorage.getItem('EMPLOYEE_DATA');	//员工数�
 var _SELLER_DATA   = window.localStorage.getItem('SELLER_DATA');	//销售数据
 
 //20160612 根据郑波要求的“如果没有销售人员也需要显示华天国旅的默认销售面板信息”，所以将默认值修改为华天旅游网的默认信息
-var _SHOW_DATA     = '华天国旅,073155555555,http://www.htyou.com/weixin_h5/images/default_face.png,0' //显示数据，该数据用来显示销售员面板和分享者ID，默认为空，待根据算法赋予其值
+var _SHOW_DATA = '华天国旅,073155555555,http://www.htyou.com/weixin_h5/images/default_face.png,0' //显示数据，该数据用来显示销售员面板和分享者ID，默认为空，待根据算法赋予其值
 
 //查询url参数函数
 //有则返回参数列表list
 //没有则返回空串
-function getParameters(inUrl/*完整的URL字符串*/){
+function getParameters(inUrl/*完整的URL字符串*/) {
 	//url中有?号才继续
-	if (inUrl.indexOf('\?')>=0){
-		return inUrl.substring(inUrl.indexOf('?')+1).split('&');    //有则返回所有参数的list
-	}else{
+	if (inUrl.indexOf('\?') >= 0) {
+		return inUrl.substring(inUrl.indexOf('?') + 1).split('&');    //有则返回所有参数的list
+	} else {
 		return '';  //没有则返回''
 	}
 }
@@ -48,16 +47,16 @@ function getParameters(inUrl/*完整的URL字符串*/){
 //获得url中某个参数的值
 //有则返回参数的值
 //没有则返回空串
-function getParameterValue(inUrl/*输入Url*/, inName/*参数名*/){
+function getParameterValue(inUrl/*输入Url*/, inName/*参数名*/) {
 	var paraList = getParameters(inUrl);
-	for (var i=0; i<paraList.length; i++){
+	for (var i = 0; i < paraList.length; i++) {
 		//如果没有'='则跳过
-		if (paraList[i].indexOf('=')<0){
+		if (paraList[i].indexOf('=') < 0) {
 			continue;
 		}
 		//如果参数名=inName则返回参数值
 		var tempVal = paraList[i].split('=');
-		if (tempVal[0]==inName){
+		if (tempVal[0] == inName) {
 			return tempVal[1];
 		}
 	}
@@ -66,67 +65,71 @@ function getParameterValue(inUrl/*输入Url*/, inName/*参数名*/){
 //TEST: getParameterValue('http://www.htyou.com/index.html?q=北京&asdf=123&zxcv=123&a=fasdf','q');
 
 //底部上划载入特效
-function slide(hrf){
+function slide(hrf) {
 	var options = {
-		"href"           : hrf,
-		"direction"      : "up", // 'left|right|up|down', default 'left' (which is like 'next')
-		"duration"       :  1000, // in milliseconds (ms), default 400
-		"slowdownfactor" :    4, // overlap views (higher number is more) or no overlap (1), default 3
-		"iosdelay"       :  100, // ms to wait for the iOS webview to update before animation kicks in, default 50
-		"androiddelay"   :  50  // same as above but for Android, default 50
+		"href":           hrf,
+		"direction":      "up", // 'left|right|up|down', default 'left' (which is like 'next')
+		"duration":       1000, // in milliseconds (ms), default 400
+		"slowdownfactor": 4, // overlap views (higher number is more) or no overlap (1), default 3
+		"iosdelay":       100, // ms to wait for the iOS webview to update before animation kicks in, default 50
+		"androiddelay":   50  // same as above but for Android, default 50
 	};
 	window.plugins.nativepagetransitions.slide(
 		options,
-		function (msg) {console.log("success: "+msg)}, // called when the animation has finished
-		function (msg) {alert("error: "+msg)} // called in case you pass in weird values
+		function (msg) {
+			console.log("success: " + msg)
+		}, // called when the animation has finished
+		function (msg) {
+			alert("error: " + msg)
+		} // called in case you pass in weird values
 	);
 }
 
 //翻转载入动画特效
 function flip(href) {
 	window.plugins.nativepagetransitions.flip({
-		'duration': 1000,
-		'direction': 'right',
-		'iosdelay': 100,
-		'androiddelay': 50,
+		'duration':      1000,
+		'direction':     'right',
+		'iosdelay':      100,
+		'androiddelay':  50,
 		'winphonedelay': 800,
-		'href': href
+		'href':          href
 	});
 }
 
 //设置返回链接
-function setBack(inSelecter){
-	$(inSelecter).attr('href',document.referrer);
+function setBack(inSelecter) {
+	$(inSelecter).attr('href', document.referrer);
 }
 //执行返回
-function doBack(){
+function doBack() {
 	/*window.location.href= document.referrer;*/
-	var pathLog     = window.localStorage.getItem('PATH_LOG');
-	var lastPath    = pathLog.split(',')[pathLog.split(',').length-1];  //最后一条记录的URL
+	var pathLog  = window.localStorage.getItem('PATH_LOG');
+	var lastPath = pathLog.split(',')[pathLog.split(',').length - 1];  //最后一条记录的URL
 	//如果当前页面在队列末尾，则删除当前页
-	if (lastPath==window.location.href){
-		window.localStorage.setItem('PATH_LOG',pathLog.substring(0,pathLog.lastIndexOf(',')));
-		lastPath    = window.localStorage.getItem('PATH_LOG').split(',')[window.localStorage.getItem('PATH_LOG').split(',').length-1];
+	if (lastPath == window.location.href) {
+		window.localStorage.setItem('PATH_LOG', pathLog.substring(0, pathLog.lastIndexOf(',')));
+		lastPath = window.localStorage.getItem('PATH_LOG').split(',')[window.localStorage.getItem('PATH_LOG').split(',').length - 1];
 	}
 	window.location.href = lastPath;
 }
 
 //需要JQuery，匿名自执行函数，用来控制历史纪录
-(function(){
+(function () {
 	//到首页后将清空路径记录（考虑首页没有回退，而且首页作为起点）
-	if (window.location.href.indexOf('main.html')!=-1){
-		window.localStorage.setItem('PATH_LOG','');
+	if (window.location.href.indexOf('main.html') != -1) {
+		window.localStorage.setItem('PATH_LOG', '');
 	}
 	//如果没有PATH_LOG则初始化
-	if ( window.localStorage.getItem('PATH_LOG')==null){
-		window.localStorage.setItem('PATH_LOG','');
+	if (window.localStorage.getItem('PATH_LOG') == null) {
+		window.localStorage.setItem('PATH_LOG', '');
 	}
 	//console.log('[Path log START]')
-	var pathLog     = window.localStorage.getItem('PATH_LOG');
-	var lastPath    = pathLog.split(',')[pathLog.split(',').length-1];  //最后一条记录的URL
+	var pathLog  = window.localStorage.getItem('PATH_LOG');
+	var lastPath = pathLog.split(',')[pathLog.split(',').length - 1];  //最后一条记录的URL
 	//如果最后一条记录不是自己，就把自己加到队列末尾
-	if (window.location.href != lastPath){
-		window.localStorage.setItem( 'PATH_LOG',pathLog+','+window.location.href);
+	if (window.location.href != lastPath) {
+		window.localStorage.setItem('PATH_LOG', pathLog + ',' + window.location.href);
 	}
 	//后台打印路径记录
 	/*var pathList    =  window.localStorage.getItem('PATH_LOG').split(',');
@@ -135,7 +138,6 @@ function doBack(){
 	 console.log(pathList[i]);
 	 }*/
 })();
-
 
 
 //Caojun 20160108 新增:销售人员信息读取函数
@@ -163,7 +165,7 @@ function doBack(){
 					//将数据存储到_EMPLOYEE_DATA变量中
 					_EMPLOYEE_DATA = window.localStorage.getItem('EMPLOYEE_DATA');
 				}
-			}else{	//20160526 郑波自己修改了数据库数据让自己身份变更，这样结果就取不到数据，但还是出现他以前销售员信息的显示，针对此问题，如果碰到数据为空，将清除掉localstorage中的销售员信息
+			} else {	//20160526 郑波自己修改了数据库数据让自己身份变更，这样结果就取不到数据，但还是出现他以前销售员信息的显示，针对此问题，如果碰到数据为空，将清除掉localstorage中的销售员信息
 				window.localStorage.removeItem('EMPLOYEE_DATA');
 			}
 		});
@@ -279,18 +281,15 @@ function doBack(){
 // })(window);
 
 
-
-
-
 //判断页面处于何种浏览器框架下
 //包括:weixin,cordova,browser
-function whereami(){
-	var userAgent   = navigator.userAgent.toLowerCase();
+function whereami() {
+	var userAgent = navigator.userAgent.toLowerCase();
 	//微信
-	if (userAgent.match(/micromessenger/i)=='micromessenger')
+	if (userAgent.match(/micromessenger/i) == 'micromessenger')
 		return 'weixin';
 	//Cordova
-	if (userAgent.match(/Crosswalk/i)=='crosswalk')
+	if (userAgent.match(/Crosswalk/i) == 'crosswalk')
 		return 'cordova';
 	return 'other'
 }
@@ -298,7 +297,7 @@ function whereami(){
 
 
 // 配置jquery,requirejs语法
-if (typeof(avalone)=='function') {
+if (typeof(avalone) == 'function') {
 	avalon.config({
 		paths: {
 			/*jquery: '../jquery/dist/jquery.min.js',*/
@@ -310,25 +309,25 @@ if (typeof(avalone)=='function') {
 
 
 // 判断用户设备类型
-function deviceType(){
+function deviceType() {
 	//var info = window.navigator;
-	var platform	= navigator.platform.toLowerCase();
-	var useragent	= navigator.userAgent.toLowerCase();
+	var platform  = navigator.platform.toLowerCase();
+	var useragent = navigator.userAgent.toLowerCase();
 
 	//是Windows的判断
-	if (platform.indexOf('win32')>-1){
+	if (platform.indexOf('win32') > -1) {
 		return "Windows";
 	}
 	//是Android的判断
-	if (platform.indexOf('linux')>-1 && useragent.indexOf('android')){
+	if (platform.indexOf('linux') > -1 && useragent.indexOf('android')) {
 		return "Android";
 	}
 	//是iPhone的判断
-	if (useragent.indexOf('iphone')>-1){
+	if (useragent.indexOf('iphone') > -1) {
 		return "iPhone";
 	}
 	//是iPad的判断
-	if (useragent.indexOf('ipad')>-1){
+	if (useragent.indexOf('ipad') > -1) {
 		return "iPad";
 	}
 
@@ -337,32 +336,33 @@ function deviceType(){
 }
 
 //20160530 因为在微信上突然出现找不到函数deviceType的BUG，考虑尝试将该函数添加到window中调用。故做如下代码
-(function(window){
-	function deviceType4Window(){
+(function (window) {
+	function deviceType4Window() {
 		//var info = window.navigator;
-		var platform	= navigator.platform.toLowerCase();
-		var useragent	= navigator.userAgent.toLowerCase();
+		var platform  = navigator.platform.toLowerCase();
+		var useragent = navigator.userAgent.toLowerCase();
 
 		//是Windows的判断
-		if (platform.indexOf('win32')>-1){
+		if (platform.indexOf('win32') > -1) {
 			return "Windows";
 		}
 		//是Android的判断
-		if (platform.indexOf('linux')>-1 && useragent.indexOf('android')){
+		if (platform.indexOf('linux') > -1 && useragent.indexOf('android')) {
 			return "Android";
 		}
 		//是iPhone的判断
-		if (useragent.indexOf('iphone')>-1){
+		if (useragent.indexOf('iphone') > -1) {
 			return "iPhone";
 		}
 		//是iPad的判断
-		if (useragent.indexOf('ipad')>-1){
+		if (useragent.indexOf('ipad') > -1) {
 			return "iPad";
 		}
 
 		//其它设备返回Ohter
 		return "Other";
 	}
+
 	window.dType = deviceType4Window;
 })(window);
 
@@ -370,16 +370,16 @@ function deviceType(){
 //逻辑：如果用户是微信登录，则在index.html就会跳转到微信授权页面，授权完毕以后会跳转到main.html并传送openid和accesstoken两个参数，需要用这两个参数获取用户信息并保存到localStorage中。
 //功能：如果在main.html页面发现附带有参数：openid和accesstoken，则调用接口获取用户信息并保存到localStorage中
 //20160314 曹熙又变了，现在这个接口要用id来取
-(function(window){
-	if (window.location.href.indexOf('main.html')>-1 || window.location.href.indexOf('user-info.html')>-1){
+(function (window) {
+	if (window.location.href.indexOf('main.html') > -1 || window.location.href.indexOf('user-info.html') > -1) {
 		//如果参数中有from=表示是从微信中分享过来的，这样的情况不重新保存用户信息到USER_DATA;
-		if (getParameterValue(window.location.href, 'from')!=''){
+		if (getParameterValue(window.location.href, 'from') != '') {
 			return false;
 		}
-		var userid      = getParameterValue(window.location.href, 'id');
+		var userid = getParameterValue(window.location.href, 'id');
 		//userid必须有，并且不为空才能调用接口获取用户数据
-		if (userid!=''){
-			$.getJSON(server_addr+'/user/htuser_getGuestsInfoById.action?userid='+userid, function (result) {
+		if (userid != '') {
+			$.getJSON(server_addr + '/user/htuser_getGuestsInfoById.action?userid=' + userid, function (result) {
 				//从接口获得的用户数据保存到localStorage中
 				window.localStorage.setItem('USER_DATA', JSON.stringify(result.guestVO));
 			});
@@ -405,7 +405,7 @@ function showSellerUI(inDATA) {
 	//首页的处理方式
 	if (window.location.href.indexOf('main.html') > -1) {
 		//20160606 郑波要求无销售信息则默认显示华天国旅的漂浮信息，所以首页判断如果_SHOW_DATA的开始不是'华天国旅'则才显示顶部信息并推出，是则继续走到下一步显示漂浮信息
-		if(inDATA.indexOf('华天国旅')!=0){
+		if (inDATA.indexOf('华天国旅') != 0) {
 			$('#seller-panel').height(parseInt($(window).width() / 1000 * 657));
 			$('#seller-panel .seller-panel-bg').height(parseInt($(window).width() / 1000 * 657));
 			//$('#seller-panel .seller-panel-bg').css({'background':'url('+inDATA.split(',')[2]+') 50% 50% no-repeat','background-size':'cover'});
@@ -425,37 +425,56 @@ function showSellerUI(inDATA) {
 	$('body').append('<div id="seller-section"></div>');
 	$('#seller-section').load('seller.html', function () {
 		//20160612 根据郑波的要求在华天国旅默认下才显示在线客服
-		if(inDATA.indexOf('华天国旅')!=0){
+		if (inDATA.indexOf('华天国旅') != 0) {
 			$('#seller .content a').eq(1).remove();
 		}
-		$('#seller .face').css({'background': 'url(' + inDATA.split(',')[2] + ') 50% 0% no-repeat','background-size':'cover'});
+		$('#seller .face').css({
+			'background':      'url(' + inDATA.split(',')[2] + ') 50% 0% no-repeat',
+			'background-size': 'cover'
+		});
 		$('#seller .content strong').text(inDATA.split(',')[0]);
 		$('#seller .content a').eq(0).attr('href', 'tel:' + inDATA.split(',')[1]);
 		$('#seller').animate({'left': '1rem', 'bottom:': '2rem'});
 	});
 }
-$(document).ready(function(){
+
+//调试模式
+if (window.localStorage.getItem('XLY_USERID') == 637486) {
+	alert('in @'+(new Date));
+	require(['wx'], function (wx) {
+		alert(typeof(wx));
+		//确保html运行在微信中
+		wx.ready(function(){
+			alert('weixin ok');
+		});
+	});
+}
+$(document).ready(function () {
+
 	//页面载入后延迟3000ms再读取销售、员工数据，保证异步载入数据已处理完毕
-	setTimeout(function(){
+	setTimeout(function () {
+
 		_EMPLOYEE_DATA = window.localStorage.getItem('EMPLOYEE_DATA');	//员工数据
 		_SELLER_DATA   = window.localStorage.getItem('SELLER_DATA');	//销售数据
-		console.log('_EMPLOYEE_DATA:'+_EMPLOYEE_DATA);
-		console.log('_SELLER_DATA:'+_SELLER_DATA);
+		console.log('_EMPLOYEE_DATA:' + _EMPLOYEE_DATA);
+		console.log('_SELLER_DATA:' + _SELLER_DATA);
 		//先将销售员DATA复制给展示DATA，再把员工DATA赋值给展示DATA，保证员工DATA有最高的优先级
-		if (_SELLER_DATA!=null){
+		if (_SELLER_DATA != null) {
 			_SHOW_DATA = _SELLER_DATA;
 		}
-		if (_EMPLOYEE_DATA!=null){
+		if (_EMPLOYEE_DATA != null) {
 			_SHOW_DATA = _EMPLOYEE_DATA;
 		}
 		//如果最后展示DATA还是null，则表示不用展示，下面将做处理
-		console.log('_SHOW_DATA:'+_SHOW_DATA);
-		if (_SHOW_DATA!=null && _SHOW_DATA!=''){
+		console.log('_SHOW_DATA:' + _SHOW_DATA);
+		if (_SHOW_DATA != null && _SHOW_DATA != '') {
 			showSellerUI(_SHOW_DATA);
 		}
 		// 20160201 新版微信JS-SDK接口调用
+
 		if (whereami() == 'weixin') {
 			/*alert('http://www.htyou.com/weixin/getJsConfig.action?page_url='+window.location.href);*/
+			//alert((window.localStorage.getItem('XLY_USERID') || 0));
 			//设置分享ID
 			var shareID = '';
 			if (_SHOW_DATA != null && _SHOW_DATA != '') {
@@ -463,13 +482,13 @@ $(document).ready(function(){
 			}
 			require(['wx'], function (wx) {
 				//alert('4');
-				$.getJSON(server_addr+'/weixin/getJsConfig.action?jsoncallback=?&page_url=' + window.location.href, function (result) {
+				$.getJSON(server_addr + '/weixin/getJsConfig.action?jsoncallback=?&page_url=' + window.location.href, function (result) {
 					//alert(result.timestamp+'\n'+result.appId+'\n'+result.noncestr+'\n'+result.url+'\n'+result.signature);
 					wx.config({
-						debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-						appId: result.appId, // 必填，公众号的唯一标识
+						debug:     false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+						appId:     result.appId, // 必填，公众号的唯一标识
 						timestamp: result.timestamp, // 必填，生成签名的时间戳
-						nonceStr: result.noncestr, // 必填，生成签名的随机串
+						nonceStr:  result.noncestr, // 必填，生成签名的随机串
 						signature: result.signature,// 必填，签名，见附录1
 						jsApiList: [
 							'checkJsApi',
@@ -511,79 +530,79 @@ $(document).ready(function(){
 						] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 					});
 					wx.ready(function () {
-						var title = result.guestVO.guestName+'分享给你'+$('title').text();
-						var desc = $('title').text();
-						var link = window.location.href;
+						var title  = result.guestVO.guestName + '分享给你' + $('title').text();
+						var desc   = $('title').text();
+						var link   = window.location.href;
 						var imgUrl = $('body img').eq(0).attr('src');
-						if (link.indexOf('?')){
-							link = link+'&sellerid='+(window.localStorage.getItem('XLY_USERID')||0);
-						}else{
-							link = link+'?sellerid='+(window.localStorage.getItem('XLY_USERID')||0);
+						if (link.indexOf('?')) {
+							link = link + '&sellerid=' + (window.localStorage.getItem('XLY_USERID') || 0);
+						} else {
+							link = link + '?sellerid=' + (window.localStorage.getItem('XLY_USERID') || 0);
 						}
-						console.log(title+','+desc+','+link+','+imgUrl);
+						console.log(title + ',' + desc + ',' + link + ',' + imgUrl);
 						//分享到朋友圈
 						wx.onMenuShareTimeline({
-							title: title,
-							link: link,
-							imgUrl: imgUrl,
+							title:   title,
+							link:    link,
+							imgUrl:  imgUrl,
 							success: function () {
 								alert('感谢您的分享');
 							},
-							cancel: function () {
+							cancel:  function () {
 								alert('欢迎您下次再进行分享');
 							}
 						});
 						//分享给朋友
 						wx.onMenuShareAppMessage({
-							title: title,	// 分享标题
-							desc: desc,		// 分享描述
-							link: link,		// 分享链接
-							imgUrl: imgUrl,	// 分享图标
-							type: 'link',	// 分享类型,music、video或link，不填默认为link
+							title:   title,	// 分享标题
+							desc:    desc,		// 分享描述
+							link:    link,		// 分享链接
+							imgUrl:  imgUrl,	// 分享图标
+							type:    'link',	// 分享类型,music、video或link，不填默认为link
 							dataUrl: '',	// 如果type是music或video，则要提供数据链接，默认为空
 							success: function () {
 								alert('感谢您的分享');
 							},
-							cancel: function () {
+							cancel:  function () {
 								alert('欢迎您下次再进行分享');
 							}
 						});
 						//分享到QQ
 						wx.onMenuShareQQ({
-							title: title,	// 分享标题
-							desc: desc,		// 分享描述
-							link: link,		// 分享链接
-							imgUrl: imgUrl,	// 分享图标
+							title:   title,	// 分享标题
+							desc:    desc,		// 分享描述
+							link:    link,		// 分享链接
+							imgUrl:  imgUrl,	// 分享图标
 							success: function () {
 								alert('感谢您的分享');
 							},
-							cancel: function () {
+							cancel:  function () {
 								alert('欢迎您下次再进行分享');
 							}
 						});
 						//分享到腾讯微博
 						wx.onMenuShareWeibo({
-							title: title,	// 分享标题
-							desc: desc,		// 分享描述
-							link: link,		// 分享链接
-							imgUrl: imgUrl,	// 分享图标
+							title:   title,	// 分享标题
+							desc:    desc,		// 分享描述
+							link:    link,		// 分享链接
+							imgUrl:  imgUrl,	// 分享图标
 							success: function () {
 								alert('感谢您的分享');
 							},
-							cancel: function () {
+							cancel:  function () {
 								alert('欢迎您下次再进行分享');
 							}
 						});
 						//分享到QQ空间
 						wx.onMenuShareQZone({
-							title: title,	// 分享标题
-							desc: desc,		// 分享描述
-							link: link,		// 分享链接
-							imgUrl: imgUrl,	// 分享图标
+							title:   title,	// 分享标题
+							desc:    desc,		// 分享描述
+							link:    link,		// 分享链接
+							imgUrl:  imgUrl,	// 分享图标
 							success: function () {
 								alert('感谢您的分享');
 							},
-							cancel: function () {
+							cancel:  function () {
 								alert('欢迎您下次再进行分享');
 							}
 						});
@@ -591,7 +610,7 @@ $(document).ready(function(){
 				});
 			});
 		}
-	},0);
+	}, 3000);
 });
 // $.getJSON('/user/htuser_getGuestsInfoById.action?userid=626685', function (result) {
 // 	//从接口获得的用户数据保存到localStorage中
@@ -599,12 +618,11 @@ $(document).ready(function(){
 // });
 
 
-
 //低版本浏览器屏蔽console.log，再也不用因为某个console.log没干掉在低版本浏览器上出问题了
-(function(){
-	if (typeof(console)!="object"){
+(function () {
+	if (typeof(console) != "object") {
 		window.console = {
-			log:function(inStr){
+			log: function (inStr) {
 				//alert(inStr);
 			}
 		};
@@ -617,8 +635,8 @@ $(document).ready(function(){
  * 小驴友项目用js代码起始段
  */
 
-// 将userid更新保存到localStorage中去
-var userid = getParameterValue(window.location.href,'userid');
-if (userid!=''){
-	window.localStorage.setItem('XLY_USERID',userid);
+	// 将userid更新保存到localStorage中去
+var userid = getParameterValue(window.location.href, 'userid');
+if (userid != '') {
+	window.localStorage.setItem('XLY_USERID', userid);
 }
